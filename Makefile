7547b60349de0d341f8a5ea33896b8779b91d112
Léo Gillot-Lamure <lgillot-@student.42.fr>
2015-01-31 18:32:44 +0100

    add memchr

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 NAME = libft.a
-OBJS = ft_memset.o ft_bzero.o ft_memcpy.o ft_memmove.o
+OBJS = ft_memset.o ft_bzero.o ft_memcpy.o ft_memmove.o ft_memchr.o
 CFLAGS = -Wall -Wextra -Werror
 
 all: $(NAME)
--- ft_memchr.c
+++ ft_memchr.c
@@ -1,24 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lgillot- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2015/01/24 22:20:52 by lgillot-          #+#    #+#             */
-/*   Updated: 2015/01/31 18:23:14 by lgillot-         ###   ########.fr       */
+/*   Created: 2015/01/31 18:18:07 by lgillot-          #+#    #+#             */
+/*   Updated: 2015/01/31 18:31:06 by lgillot-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include <string.h>
 
-# include <string.h>
+#include "libft.h"
 
-void	*ft_memset(void *b, int c, size_t len);
-void	ft_bzero(void *s, size_t n);
-void	*ft_memcpy(void *dst, const void *src, size_t n);
-void	*ft_memmove(void *dst, const void *src, size_t n);
-void	*ft_memchr(const void *s, int c, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	const void *s_it;
 
-#endif
+	s_it = s;
+	while (s_it < s + n)
+	{
+		if (*(const unsigned char *)s_it == (unsigned char)c)
+		{
+			return (void *)s_it;
+		}
+		s_it++;
+	}
+	return (NULL);
+}
