9f3e0ec13bf3e7bd00459b7ee4b6378f74389da6
Léo Gillot-Lamure <lgillot-@student.42.fr>
2015-04-22 01:22:51 +0200

    add strlen

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: lgillot- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/01/24 22:20:52 by lgillot-          #+#    #+#             */
-/*   Updated: 2015/04/22 00:22:52 by lgillot-         ###   ########.fr       */
+/*   Updated: 2015/04/22 01:17:06 by lgillot-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,6 @@ void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 char	*ft_strcpy(char *dst, const char *src);
+size_t	ft_strlen(const char *s);
 
 #endif
