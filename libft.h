e556d2ffe607990130c2d5e66998ae45a83c5030
Léo Gillot-Lamure <lgillot-@student.42.fr>
2015-04-22 01:03:13 +0200

    add strcpy

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: lgillot- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/01/24 22:20:52 by lgillot-          #+#    #+#             */
-/*   Updated: 2015/01/31 18:48:08 by lgillot-         ###   ########.fr       */
+/*   Updated: 2015/04/22 00:22:52 by lgillot-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,6 @@ void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
+char	*ft_strcpy(char *dst, const char *src);
 
 #endif
