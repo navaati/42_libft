bd7c1d4b890bc80361d95ae3403b97db5aa4a469
Léo Gillot-Lamure <lgillot-@student.42.fr>
2015-05-07 22:00:04 +0200

    add strncmp

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: lgillot- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/01/24 22:20:52 by lgillot-          #+#    #+#             */
-/*   Updated: 2015/05/07 21:37:46 by lgillot-         ###   ########.fr       */
+/*   Updated: 2015/05/07 21:45:20 by lgillot-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,5 +39,6 @@ char	*ft_strrchr(const char *s, int c);
 char	*ft_strstr(const char *s1, const char *s2);
 char	*ft_strnstr(const char *s1, const char *s2, size_t n);
 int		ft_strcmp(const char *s1, const char *s2);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 
 #endif
